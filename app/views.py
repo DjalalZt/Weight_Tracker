81360c24a512b3375aa5ab1df71d3641de2d748b
Zitouni Terki Mohamed Djalal Islam <djallelzitouniterki@gmail.com>
2021-03-27 19:29:16 +0100

    Finished the add page and function

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Weights
 from . import db
 import json
 
@@ -13,10 +13,23 @@ def home():
     return render_template("home.html", user=current_user)
 
 
-@views.route('/add')
+@views.route('/add', methods=['POST', 'GET'])
 @login_required
-def add():
-    return render_template("add.html", user=current_user)
+def add(): 
+    if request.method == 'GET':
+        return render_template("add.html", user=current_user)
+    else:
+        weight = request.form.get('add')
+        num_weight = int(weight)
+        if num_weight > 0:
+            flash('Weight added successfully!', category='success')
+            new_weight = Weights(weight=num_weight, user_id=current_user.id)
+            db.session.add(new_weight)
+            db.session.commit()
+            return redirect(url_for('views.add'))
+        else:
+            flash('Please enter an non null positive number', category='error')
+            return redirect(url_for('views.add'))    
 
 
 @views.route('/show')
@@ -25,14 +38,14 @@ def show():
     return render_template("show.html", user=current_user)
 
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
+##@views.route('/delete-note', methods=['POST'])
+##def delete_note():
+##    note = json.loads(request.data)
+##    noteId = note['noteId']
+##    note = Note.query.get(noteId)
+##    if note:
+##        if note.user_id == current_user.id:
+##            db.session.delete(note)
+##            db.session.commit()
             
-    return jsonify({})
+##    return jsonify({})
